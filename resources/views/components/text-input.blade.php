167feafa050afa0552ba2ba5f457c1948e234a83
Pedro2804 <pedro28hdez@hotmail.com>
2024-05-03 17:36:43 -0600

    Cambio de color

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,6 +1,6 @@
 @props(['disabled' => false])
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' =>
-                                                                  'border-black text-gray-700 bg-orange-100
+                                                                  'border-black text-black bg-orange-100
                                                                    focus:border-orange-700 focus:ring-orange-400
                                                                    rounded-md shadow-sm']) !!}>
