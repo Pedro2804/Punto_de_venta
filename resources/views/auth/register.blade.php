aa79f619a6ded87c595317b4e0cbbac25510ddce
Pedro2804 <127828585+Pedro2804@users.noreply.github.com>
2024-06-30 20:09:44 -0600

    Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}" novalidate>
+    <form method="POST" action="{{ route('register.store') }}" novalidate>
         @csrf
 
         <!-- Name -->
